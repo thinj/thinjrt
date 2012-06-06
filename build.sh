73e80ffa0a2a86b491d58ada694ff786980dc399
Henrik Jaeger <thinjaeger@gmail.com>
2012-06-06 19:12:47 +0200

    First commit

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+DESTINATION=/tools/thinj/devel
+LIBDIR=$DESTINATION/lib
+
+mkdir -p $DESTINATION || exit 1
+mkdir -p $LIBDIR || exit 1
+
+
+javac -sourcepath src -d bin `find src -name "*.java" -print` || exit 1
+
+jar cf $LIBDIR/thinjrt.jar -C bin java || exit 1
+
